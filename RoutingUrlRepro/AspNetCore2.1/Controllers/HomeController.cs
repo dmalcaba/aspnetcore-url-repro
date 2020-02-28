c8857a3911f7173a71970eab35f351994ff212ea
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-02-27 16:08:53 -0800

    Initial commit

---
+++ RoutingUrlRepro/AspNetCore2.1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore2._1.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("{recordId}/Edit")]
+        public IActionResult EditRecord(Guid recordId)
+        {
+            string url1 = Url.Action(nameof(UpdateRecord), "Home");
+            string url2 = Url.Action(nameof(ViewRecord), "Home");
+
+            var res=  $"UpdateRecord: '{url1}'\nViewRecord: '{url2}'";
+
+            return Ok(res);
+        }
+
+        [HttpGet("{recordId}/View")]
+        public IActionResult ViewRecord(Guid recordId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{recordId}")]
+        public IActionResult UpdateRecord(Guid recordId)
+        {
+            return Ok();
+        }
+
+
+        public IActionResult Index()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
---
+++ RoutingUrlRepro/AspNetCore3.1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNetCore3._1.Models;
+
+namespace AspNetCore3._1.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("{recordId}/Edit")]
+        public IActionResult EditRecord(Guid recordId)
+        {
+            string url1 = Url.Action(nameof(UpdateRecord), "Home");
+            string url2 = Url.Action(nameof(ViewRecord), "Home");
+
+            var res = $"UpdateRecord: '{url1}'\nViewRecord: '{url2}'";
+
+            return Ok(res);
+        }
+
+        [HttpGet("{recordId}/View")]
+        public IActionResult ViewRecord(Guid recordId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{recordId}")]
+        public IActionResult UpdateRecord(Guid recordId)
+        {
+            return Ok();
+        }
+
+
+        public IActionResult Index()
+        {
+            return Ok("Hello world!");
+        }
+
+    }
+}
